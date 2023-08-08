51fa1536d46a86b3161f65214b45f0b07e3826e6
GustavoSchmitt <gustavo.schmitt1998@gmail.com>
2023-08-08 01:04:53 -0300

    pequena revisão

--- script.js
+++ script.js
@@ -1,144 +1,104 @@
 
-let produtos = '{"produtos": ['+ 
-'{"imagem":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Jb9dKcIntVwjLYC1ClftwzO-qE28aeBDjQ&usqp=CAU","nome_do_produto":"teclado mecânico","preco":309.99,"entrega_gratis":true,"promocao":"350.00","descricao":"Produto em perfeito estado, com switches Cherry MX","marca":"Ichi","condicao":"Novo","categoria":"Teclado"},' + 
-'{"imagem":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv6N5wTTLmU_eNwsYtb3UtQ_w8k4CaIkx7bHbNLcEds8Iezyp1BicmpMvyvCYb98fBvy8&usqp=CAU","nome_do_produto":"headset gamer","preco":149.9,"entrega_gratis":false,"promocao":false,"descricao":"Produto inovador, com cancelamento de ruído e microfone removível","marca":"Ni","condicao":"Novo","categoria":"Headset"},' + 
-'{"imagem":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGq4WQ_nGsq6MzfYNEOmjhpmDbnNBPXXrZJA&usqp=CAU","nome_do_produto":"jogo ninja Kiwi","preco":89.9,"entrega_gratis":true,"promocao":false,"descricao":"Produto em perfeito estado, com temática de estratégia medieval","marca":"San","condicao":"Novo","categoria":"Jogo"},' + 
-'{"imagem":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-z1Q1KWuHqNU_PKTA12fyqM2HtnqTC9X5kg&usqp=CAU","nome_do_produto":"mouse sem fio","preco":59.99,"entrega_gratis":true,"promocao":"80.00","descricao":"Produto em perfeito estado, com sensor óptico de alta precisão","marca":"Yon","condicao":"Seminovo","categoria":"Mouse"},' + 
-'{"imagem":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Jb9dKcIntVwjLYC1ClftwzO-qE28aeBDjQ&usqp=CAU","nome_do_produto":"Teclado Gamer","preco":309.99,"entrega_gratis":true,"promocao":false,"descricao":"Produto inovador","marca":"Ichi","condicao":"Novo","categoria":"Teclado"},' + 
-'{"imagem":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv6N5wTTLmU_eNwsYtb3UtQ_w8k4CaIkx7bHbNLcEds8Iezyp1BicmpMvyvCYb98fBvy8&usqp=CAU","nome_do_produto":"Headset Bluetooth","preco":"50.00","entrega_gratis":false,"promocao":"71.00","descricao":"Produto em perfeito estado","marca":"Ni","condicao":"Seminovo","categoria":"Headset"},' + 
-'{"imagem":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGq4WQ_nGsq6MzfYNEOmjhpmDbnNBPXXrZJA&usqp=CAU","nome_do_produto":"Jogo ninja Kiwi","preco":139.9,"entrega_gratis":true,"promocao":false,"descricao":"Produto em perfeito estado","marca":"San","condicao":"Novo","categoria":"Jogo"},' + 
-'{"imagem":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-z1Q1KWuHqNU_PKTA12fyqM2HtnqTC9X5kg&usqp=CAU","nome_do_produto":"Mouse Wireless","preco":80,"entrega_gratis":false,"promocao":"120.50","descricao":"Produto inovador","marca":"Yon","condicao":"Seminovo","categoria":"Mouse"},' + 
-'{"imagem":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Jb9dKcIntVwjLYC1ClftwzO-qE28aeBDjQ&usqp=CAU","nome_do_produto":"Teclado Mecânico","preco":499.99,"entrega_gratis":true,"promocao":false,"descricao":"Produto inovador","marca":"Ichi","condicao":"Novo","categoria":"Teclado"},'+ 
-'{"imagem":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv6N5wTTLmU_eNwsYtb3UtQ_w8k4CaIkx7bHbNLcEds8Iezyp1BicmpMvyvCYb98fBvy8&usqp=CAU","nome_do_produto":"Headset Gamer","preco":120,"entrega_gratis":false,"promocao":"145.90","descricao":"Produto inovador","marca":"Yon","condicao":"Seminovo","categoria":"Headset"}' + ']}';
+let produtos =  [
+{imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Jb9dKcIntVwjLYC1ClftwzO-qE28aeBDjQ&usqp=CAU",nome_do_produto:"teclado mecânico",preco:309.99,entrega_gratis:true,promocao:"350.00",descricao:"Produto em perfeito estado, com switches Cherry MX",marca:"Ichi",condicao:"Novo",categoria:"Teclado"}, 
+{imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv6N5wTTLmU_eNwsYtb3UtQ_w8k4CaIkx7bHbNLcEds8Iezyp1BicmpMvyvCYb98fBvy8&usqp=CAU",nome_do_produto:"headset gamer",preco:149.9,entrega_gratis:false,promocao:false,descricao:"Produto inovador, com cancelamento de ruído e microfone removível",marca:"Ni",condicao:"Novo",categoria:"Headset"}, 
+{imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGq4WQ_nGsq6MzfYNEOmjhpmDbnNBPXXrZJA&usqp=CAU",nome_do_produto:"jogo ninja Kiwi",preco:89.9,entrega_gratis:true,"promocao":false,descricao:"Produto em perfeito estado, com temática de estratégia medieval",marca:"San",condicao:"Novo",categoria:"Jogo"}, 
+{imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-z1Q1KWuHqNU_PKTA12fyqM2HtnqTC9X5kg&usqp=CAU",nome_do_produto:"mouse sem fio",preco:59.99,entrega_gratis:true,promocao:"80.00",descricao:"Produto em perfeito estado, com sensor óptico de alta precisão",marca:"Yon",condicao:"Seminovo",categoria:"Mouse"}, 
+{imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Jb9dKcIntVwjLYC1ClftwzO-qE28aeBDjQ&usqp=CAU",nome_do_produto:"Teclado Gamer",preco:309.99,entrega_gratis:true,promocao:false,descricao:"Produto inovador",marca:"Ichi",condicao:"Novo",categoria:"Teclado"}, 
+{imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv6N5wTTLmU_eNwsYtb3UtQ_w8k4CaIkx7bHbNLcEds8Iezyp1BicmpMvyvCYb98fBvy8&usqp=CAU",nome_do_produto:"Headset Bluetooth",preco:"50.00",entrega_gratis:false,promocao:"71.00",descricao:"Produto em perfeito estado",marca:"Ni",condicao:"Seminovo",categoria:"Headset"}, 
+{imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGq4WQ_nGsq6MzfYNEOmjhpmDbnNBPXXrZJA&usqp=CAU",nome_do_produto:"Jogo ninja Kiwi",preco:139.9,entrega_gratis:true,promocao:false,descricao:"Produto em perfeito estado",marca:"San",condicao:"Novo",categoria:"Jogo"}, 
+{imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-z1Q1KWuHqNU_PKTA12fyqM2HtnqTC9X5kg&usqp=CAU",nome_do_produto:"Mouse Wireless",preco:80,entrega_gratis:false,promocao:"120.50",descricao:"Produto inovador",marca:"Yon",condicao:"Seminovo",categoria:"Mouse"}, 
+{imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Jb9dKcIntVwjLYC1ClftwzO-qE28aeBDjQ&usqp=CAU",nome_do_produto:"Teclado Mecânico",preco:499.99,entrega_gratis:true,promocao:false,descricao:"Produto inovador",marca:"Ichi",condicao:"Novo",categoria:"Teclado"},
+{imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv6N5wTTLmU_eNwsYtb3UtQ_w8k4CaIkx7bHbNLcEds8Iezyp1BicmpMvyvCYb98fBvy8&usqp=CAU",nome_do_produto:"Headset Gamer",preco:120,entrega_gratis:false,promocao:"145.90",descricao:"Produto inovador",marca:"Yon",condicao:"Seminovo",categoria:"Headset"}];
 let imagem1 = ''
 
-let produtosObj=JSON.parse(produtos);
-console.log(produtosObj);
+function createElement(element, classe){
+  const elemento = document.createElement(element)
+  if(classe != undefined) elemento.classList = classe
+  return elemento
+}
+
+function createImage(src, classe){
+  const elemento = createElement('img', classe)
+  elemento.src = src
+  return elemento
+}
 
 function criarListaProdutos(produtos) {
-    let cardContainer = document.createElement("div");
-    cardContainer.classList.add("cards-container");
-    let listaProdutos = document.createElement('ul');
-    listaProdutos.classList.add("row")
+    const cardContainer = createElement('div', "cards-container")
+
+    let listaProdutos = createElement('ul', "row")
   
     // Loop através dos produtos do JSON
-    produtos.produtos.forEach((produto) => {
+    produtos.forEach((produto) => {
     // Cria um elemento de lista
-        let itemLista = document.createElement('li');
-        itemLista.classList.add("card")
-        itemLista.classList.add("col-4")
-        itemLista.classList.add(produto.marca)
-        itemLista.classList.add(produto.condicao)
-        itemLista.classList.add(produto.categoria)
-        
-        if(produto.entrega_gratis === true){
-            itemLista.classList.add("entrega")      
-        }else{
-            itemLista.classList.add("naoEntrega")
-        }
-        if(produto.promocao != false){
-            itemLista.classList.add("promocao")
-        }else{
-            itemLista.classList.add("semPromocao")
-        }
+      const itemLista = createElement('li', `card col-4 ${produto.marca} ${produto.condicao} ${produto.categoria}`)
+      
+      produto.entrega_gratis ? itemLista.classList.add("entrega") : itemLista.classList.add("naoEntrega") 
+      produto.promocao ? itemLista.classList.add("promocao") : itemLista.classList.add("semPromocao")
 
     // Cria elementos HTML para as informações do produto
-        let nomeProduto = document.createElement('h2');
+        const nomeProduto = createElement('h2', "card-title")
         nomeProduto.textContent = produto.nome_do_produto;
-        nomeProduto.classList.add("card-title")
 
-        let novoProduto = document.createElement('span')
-        if(produto.condicao == "Novo"){
-          novoProduto.textContent = produto.condicao 
-        }
-        novoProduto.classList.add('card-new')
-  
-        let imagemProduto = document.createElement('img');
-        imagemProduto.src = produto.imagem;
-        imagemProduto.classList.add("card-image");
-
-        let precoAntes = document.createElement("span");
-        let promocao = document.createElement("span")
-        if(produto.promocao != false){
-            precoAntes.textContent = "R$ " + produto.promocao;
-            promocao.classList.add('card-promocao')
-            promocao.textContent =  ( 100 - (produto.preco / (produto.promocao / 100))).toFixed(1) + "% OFF";
-        }
-        precoAntes.classList.add('card-old-price')
+        const novoProduto = createElement('span', "card-new")
+        
+        if(produto.condicao == "Novo") novoProduto.textContent = produto.condicao 
         
         
-        let rs = document.createElement('span');
+        const imagemProduto = createImage(produto.imagem, "card-image")
+
+        const precoAntes = createElement('span', 'card-old-price')
+        const promocao = document.createElement("span")
+        
+        if(!!produto.promocao){
+          precoAntes.textContent = "R$ " + produto.promocao;
+          promocao.classList.add('card-promocao')
+          promocao.textContent =  ( 100 - (produto.preco / (produto.promocao / 100))).toFixed(1) + "% OFF";
+        }
+
+        
+        const rs = createElement('span', 'rs')
         rs.textContent = "R$ " ;
-        rs.classList.add('rs')
 
-        let precoProduto = document.createElement('span');
+        const precoProduto = createElement('span', 'card-price')
         precoProduto.textContent = produto.preco;
-        precoProduto.classList.add("card-price");
-        //precoProduto.classList.add('col-6')
         
-        let divPreco = document.createElement('div');
-        divPreco.classList.add('divPreco')
-        divPreco.appendChild(rs)
-        divPreco.appendChild(precoProduto);
-        divPreco.appendChild(promocao)
+        const divPreco = createElement('div', 'divPreco')
 
-        let marca = document.createElement('span');
+        divPreco.append(rs, precoProduto, promocao)
+
+        const marca = createElement('span', 'card-marca')
         marca.textContent = produto.marca;
-        marca.classList.add("card-marca");
         
-        let entregaG = document.createElement('span');
+        const entregaG = createElement('span')
+
         if(produto.entrega_gratis === true){
             entregaG.textContent = "Entrega grátis";
             entregaG.classList.add("card-deliver")
         }
-        let descricao = document.createElement('p');
+
+        const descricao = createElement('p', 'card-description')
         descricao.textContent = produto.descricao;
-        descricao.classList.add("card-description");
 
         // Adiciona os elementos HTML criados ao elemento de lista
-        itemLista.appendChild(nomeProduto);
-        itemLista.appendChild(novoProduto);
-        itemLista.appendChild(imagemProduto);
-        itemLista.appendChild(precoAntes);
-        
-        itemLista.appendChild(divPreco);
-        itemLista.appendChild(marca);
-        itemLista.appendChild(descricao)
-        itemLista.appendChild(entregaG);
+        itemLista.append(nomeProduto, novoProduto, imagemProduto, precoAntes, divPreco, marca, descricao, entregaG);
         
         // Adiciona o elemento de lista à lista de produtos
-        listaProdutos.appendChild(itemLista);
+        listaProdutos.append(itemLista);
     });
     
     // Adiciona a lista de produtos ao corpo da página
-    let cards = document.querySelector("#cards-container")
+    const cards = document.querySelector("#cards-container")
     cardContainer.appendChild(listaProdutos);
     cards.appendChild(cardContainer);
 
 }
 
 // Chama a função para criar a lista de produtos
-criarListaProdutos(produtosObj);
+criarListaProdutos(produtos);
 
 
-// Entrega grátis
-// var checkboxes = document.querySelectorAll('input[type=checkbox]');
-// checkboxes.forEach(function(checkbox) {
-//     checkbox.addEventListener('change', function() {
-    
-//     var itens = document.querySelectorAll('.card');
-//     // percorre todos os itens e verifica se têm a classe selecionada
-//     itens.forEach(function(item) {
-//       if (checkbox.checked && !item.classList.contains(checkbox.value)) {
-//           // esconde o item se o checkbox estiver marcado e ele não tiver a classe selecionada
-//           item.style.display = 'none';
-//         } else {
-//             // mostra o item se o checkbox não estiver marcado ou ele tiver a classe selecionada
-//             item.style.display = '';
-//     }}
-//     )
-//     })
-// })
 
 const marcas = document.querySelectorAll('.marca');
 const entregas = document.querySelectorAll('.entregas');
